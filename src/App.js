b99ac5bf1fa423ec1ec383e1d5ce8dfdbaf1c56f
li1195382526 <1195382526@qq.com>
2018-09-19 18:59:40 +0800

    二级页面

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 
 import './style/usage/app.scss'
-
+import {Switch,Route} from 'react-router-dom'
 import Home from './pages/home'
+
+import Second from './pages/second/index'
+import Time from './pages/second/time.js'
+import Supermark from './pages/second/supermark.js'
+import Dodm from './pages/second/dodm.js'
 
 class App extends Component {
   render() {
     return (
-      
+      <Switch>
+          <Route path="/Second" component={Second}></Route>
+          <Route path="/Time" component={Time}></Route>
+          <Route path="/Supermark" component={Supermark}></Route>
+          <Route path="/Dodm" component={Dodm}></Route>
         <Home></Home>
-    
+        </Switch>
     );
   }
 }
